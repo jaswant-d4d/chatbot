49e37e3752d9051f48b77ca6587cb65c982a7c1d
jaswant-d4d <jaswant.d4d@gmail.com>
2025-06-17 17:44:42 +0530

    minor change

--- backend/src/middleware/clearAuthToken.js
+++ backend/src/middleware/clearAuthToken.js
@@ -1,9 +1,9 @@
 const clearAuthToken = (res) => {
-    res.clearCookie('token', {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'Strict',
-    });
+    // res.clearCookie('token', {
+    //     httpOnly: true,
+    //     secure: process.env.NODE_ENV === 'production',
+    //     sameSite: 'Strict',
+    // });
 };
 
 module.exports = clearAuthToken;
