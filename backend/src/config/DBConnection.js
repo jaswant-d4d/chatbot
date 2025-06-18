20ec7a6c5245c8d7bd530f2549bc875686e75016
jaswant-d4d <jaswant.d4d@gmail.com>
2025-06-18 12:12:19 +0530

    minor updates

--- backend/src/config/DBConnection.js
+++ backend/src/config/DBConnection.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const mongoUri = `${process.env.MONGO_DB_URI}${process.env.MONGO_DB_NAME}`;
+        // const mongoUri = `${process.env.MONGO_DB_URI}${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`;
+        const mongoUri = `mongodb+srv://jaswantd4d:EG8MQyMHXXzeTrPe@cluster0.ywouyit.mongodb.net/chatbot?retryWrites=true&w=majority`;
         await mongoose.connect(mongoUri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 10000,
         });
         console.log('MongoDB connected');
     } catch (err) {
