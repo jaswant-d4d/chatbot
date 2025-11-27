d894f46a39b70746807b1ef717184720c88c2cd7
jaswant-d4d <jaswant.d4d@gmail.com>
2025-11-27 13:50:11 +0530

    empty push

--- backend/src/routes/companies.js
+++ backend/src/routes/companies.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Company = require("../models/Company");
 
+
 // List companies
 router.get("/", async (req, res) => {
   try {
